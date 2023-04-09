1afd44d7d9fa0751188af1f952509d2781a24d6b
Hersh2323 <stephenhercia@gmail.com>
2023-04-09 02:31:27 -0400

    dialogic npc interaction implementation

--- game/npc/generic_npc.gd
+++ game/npc/generic_npc.gd
@@ -4,6 +4,8 @@ export(NodePath) var node_path
 export(String) var enter_method
 export(String) var exit_method
 
+var alert_active = false
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -12,32 +14,29 @@ export(String) var exit_method
 func _ready():
 	pass # Replace with function body.
 
+func _input(event):
+	if get_node_or_null('DialogNode') == null:
+		if event.is_action_pressed("ui_accept") and alert_active:
+			get_tree().paused = true
+			var dialog = Dialogic.start('timeline-test_1')
+			dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+			dialog.connect('timeline_end', self, 'unpause')
+			add_child(dialog)
+	else:
+		printerr("DialogNode not null")
+
+func unpause(_timeline_name):
+	get_tree().paused = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta):
+	$overhead_arrow.visible = alert_active
 
 func _on_body_entered(body):
 	if body is Player:
-		if node_path:
-			if get_node_or_null(node_path) != null:
-				if get_node(node_path).has_method(enter_method):
-					get_node(node_path).call(enter_method)
-				else:
-					printerr("Trigger node missing method")
-			else:
-				printerr("Trigger couldn't get node")
-		else:
-			printerr("Trigger node path empty")
+		alert_active = true
+
 
 func _on_body_exited(body):
 	if body is Player:
-		if node_path:
-			if get_node_or_null(node_path) != null:
-				if get_node(node_path).has_method(exit_method):
-					get_node(node_path).call(exit_method)
-				else:
-					printerr("Trigger node missing method")
-			else:
-				printerr("Trigger couldn't get node")
-		else:
-			printerr("Trigger node path empty")
+		alert_active = false
